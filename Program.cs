a2bba24bfae84a97d989166c4a1ad86dba99aa7c
vicher-prog <jobvicher@yandex.ru>
2023-04-28 14:23:16 +0300

    Добавление второго задания

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿namespace VariablesAndOutput
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace VariablesAndOutput
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Задание 1. Создание переменных и вывод
+
             //Ф.И.О.
             string fullName;
             //Возраст
@@ -18,17 +22,37 @@
             float physics;
             //Результат
             string result;
+
             fullName = "Иван Иванович Иванов";
             age = 17;
             email = "ivan.ivanov@gmail.com";
+
             programming = 9.5f;
             mathematics = 8.7f;
             physics = 3.1f;
+
             result = $"ФИО: {fullName}\nВозраст: {age}\nEmail: " +
                 $"{email}\nБаллы по программированию: {programming}\nБаллы по математике:" +
-                $" {mathematics}\nБаллы по физике: {physics}";
-            Console.Write(result);
+                $"{mathematics}\nБаллы по физике: {physics}";
+            Console.Write(result + "\n\nНажмите любую кнопку");
             Console.ReadKey();
+
+            //Задание 2. Реализация подсчёта количества баллов по всем предметам
+
+            //Переменная под сумму баллов
+            float summ;
+            //Подсчёт суммы баллов
+            summ = programming + mathematics + physics;
+            //Переменная под средний балл
+            float averageScore;
+            //Вычисление среднего балла
+            averageScore = summ / 3;
+
+            result = $"Сумма баллов по всем дисциплинам: {summ:0.00}\nСреднее арифметическое всех баллов: {averageScore:0.00}";
+
+            Console.Write(result + "\n\nНажмите любую кнопку");
+            Console.ReadKey();
+
         }
     }
 }
