b23311b7e1aae9e63576503cd0026f407e152af3
vicher-prog <jobvicher@yandex.ru>
2023-04-28 13:37:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace VariablesAndOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ф.И.О.
+            string fullName;
+            //Возраст
+            byte age;
+            //Email
+            string email;
+            //Баллы по программированию
+            float programming;
+            //Баллы по математике
+            float mathematics;
+            //Баллы по физике
+            float physics;
+            //Результат
+            string result;
+            fullName = "Иван Иванович Иванов";
+            age = 17;
+            email = "ivan.ivanov@gmail.com";
+            programming = 9.5f;
+            mathematics = 8.7f;
+            physics = 3.1f;
+            result = $"ФИО: {fullName}\nВозраст: {age}\nEmail: " +
+                $"{email}\nБаллы по программированию: {programming}\nБаллы по математике:" +
+                $" {mathematics}\nБаллы по физике: {physics}";
+            Console.Write(result);
+            Console.ReadKey();
+        }
+    }
+}
